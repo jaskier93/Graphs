13ef17893d3298dd4e35fba2a35a1c7d90e6eb4a
Damian Grochowski <smg7@o2.pl>
2019-09-09 23:49:35 +0200

    scene builder basic controller, not finished

--- src/main/java/grahps/Controller.java
+++ src/main/java/grahps/Controller.java
@@ -1,4 +1,41 @@
 package grahps;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    private void chartPattern(ActiveEvent event) {
+        String pattern;
+        if (factorA.equals("0") && factorC.equals("0")) {
+            pattern = "f(x)=" + factorB;
+        } else if (factorC.equals("0")) {
+            pattern = "f(x)=" + factorA + "x+" + factorB;
+        } else {
+            pattern = "f(x)=" + factorC + "x^2" + factorA + "x+" + factorB;
+        }
+        label.setText(pattern);
+    }
+
+    @FXML
+    TextField factorA;
+
+    @FXML
+    TextField factorB;
+
+    @FXML
+    TextField factorC;
+
+    @FXML
+    TextField xMin;
+
+    @FXML
+    TextField xMax;
+
+
+    @FXML
+    Label label;
+
 }

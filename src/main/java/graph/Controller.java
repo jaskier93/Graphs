504b207876d29492d52f5ff095da77b9cbcf8c68
Damian Grochowski <smg7@o2.pl>
2019-09-21 23:52:40 +0200

    fixed problems, working version!

--- src/main/java/graph/Controller.java
+++ src/main/java/graph/Controller.java
@@ -1,15 +1,13 @@
 package graph;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 
 public class Controller {
     @FXML
@@ -26,54 +24,71 @@ public class Controller {
     Label label;
     @FXML
     Button button;
+    @FXML
+    LineChart<Number, Number> chart;
+    @FXML
+    NumberAxis xAxis;
+    @FXML
+    NumberAxis yAxis;
 
     @FXML
-    XYChart.Series<Number, Number> chart;
+    public void updateChart() {
+        XYChart.Series<Number, Number> series = getSeries();
+        chart.getData().clear();
+        chart.getData().add(series);
+    }
 
-    //Parser Text Field -> double
-    double xMax1 = Double.parseDouble(xMax.getText());
-    double xMin1 = Double.parseDouble(xMin.getText());
-    double a = Double.parseDouble(factorA.getText());
-    double b = Double.parseDouble(factorB.getText());
-    double c = Double.parseDouble(factorC.getText());
+    public void initialize() {
+        initChartProperties();
+        initInputControls();
+        XYChart.Series<Number, Number> series = getSeries();
+        chart.getData().add(series);
+    }
 
+    private XYChart.Series<Number, Number> getSeries() {
 
-    public void drawChart(Stage stage) {
-        XYChart.Series<Number, Number> series = chart;
+        double xMax1 = Double.parseDouble(xMax.getText());
+        double xMin1 = Double.parseDouble(xMin.getText());
+        double a = Double.parseDouble(factorA.getText());
+        double b = Double.parseDouble(factorB.getText());
+        double c = Double.parseDouble(factorC.getText());
+
+        XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
         series.setName("Chart");
-        double axisTick = 1;
-        final NumberAxis xAxis = new NumberAxis(xMin1, xMax1, axisTick);
-        final NumberAxis yAxis = new NumberAxis();
-        yAxis.setTickUnit(axisTick);
-        xAxis.setLabel("X Label");
-        yAxis.setLabel("Y Label");
-        final javafx.scene.chart.LineChart<Number, Number> lineChart = new javafx.scene.chart.LineChart<Number, Number>(xAxis, yAxis);
-        double y;
 
         String pattern;
         if (a == 0 && c == 0) {
             pattern = "f(x)=" + factorB;
             label.setText(pattern);
         } else if (c == 0) {
-            pattern = "f(x)=" + factorA + "x+" + factorB;
+            pattern = "f(x)=" + factorA.getText() + "x+" + factorB.getText();
             label.setText(pattern);
             for (double i = xMin1; i <= xMax1; i++) {
-                y = a * i + b;
-                series.getData().add(new XYChart.Data(i, y));
+                double y = a * i + b;
+                series.getData().add(new XYChart.Data<Number, Number>(i, y));
             }
         } else {
-            pattern = "f(x)=" + factorC + "x^2" + factorA + "x+" + factorB;
+            pattern = "f(x)=" + factorA.getText() + "x^2+" + factorB.getText() + "x+" + factorC.getText();
             label.setText(pattern);
             for (double i = xMin1; i < xMax1; i++) {
-                y = a * i * i + b * i + c;
-                series.getData().add(new XYChart.Data(i, y));
+                double y = a * i * i + b * i + c;
+                series.getData().add(new XYChart.Data<Number, Number>(i, y));
             }
         }
+        return series;
+    }
 
-        lineChart.getData().add(series);
-        Scene scene = new Scene(lineChart, 800, 700);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
+    private void initChartProperties() {
+        chart.setAnimated(false);
+        xAxis.setLabel("X Label");
+        yAxis.setLabel("Y Label");
+    }
+
+    private void initInputControls() {
+        xMax.setText("0");
+        xMin.setText("0");
+        factorA.setText("0");
+        factorB.setText("0");
+        factorC.setText("0");
     }
 }
--- src/main/java/graph/Main.java
+++ src/main/java/graph/Main.java
@@ -1,17 +1,14 @@
 package graph;
 
 import javafx.application.Application;
-import javafx.application.Platform;
+
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
-
-    private Controller controller;
 
     public static void main(String[] args) {
         launch(args);
@@ -19,19 +16,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.show();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/sample.fxml"));
-        //loader.setController(controller);
-        //controller.drawChart();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        Scene scene = new Scene(root, 800, 700);
-        stage.setResizable(false);
+        Scene scene = new Scene(root, 800, 800);
         stage.setScene(scene);
-
-        Platform.runLater(() -> {
-            controller.drawChart(stage);
-        });
-
-
+        stage.show();
     }
 }
